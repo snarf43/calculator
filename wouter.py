af9c82f6dc84d45fd77856d88ef50837044b1631
Frans de Jong <fdejong@bluewin.ch>
2017-12-02 14:20:37 +0100

    adding more pins

--- wouter.py
+++ wouter.py
@@ -7,52 +7,52 @@ import signal   # interrupt/timer library
 import RPi.GPIO as GPIO
 
 # Initialize the LCD using the pins
-lcd = LCD.Adafruit_CharLCDPlate()
+#lcd = LCD.Adafruit_CharLCDPlate()
 
 # create some custom characters
-lcd.create_char(1, [0, 27, 27, 0, 4, 4, 17, 14])  # lachend mannetje
-lcd.create_char(2, [0, 1, 3, 22, 28, 8, 0, 0])
-lcd.create_char(3, [0, 14, 21, 23, 17, 14, 0, 0])
-lcd.create_char(4, [31, 17, 10, 4, 10, 17, 31, 0])
-lcd.create_char(5, [8, 12, 10, 9, 10, 12, 8, 0])
-lcd.create_char(6, [2, 6, 10, 18, 10, 6, 2, 0])
-lcd.create_char(7, [31, 17, 21, 21, 21, 21, 17, 31])
+#lcd.create_char(1, [0, 27, 27, 0, 4, 4, 17, 14])  # lachend mannetje
+#lcd.create_char(2, [0, 1, 3, 22, 28, 8, 0, 0])
+#lcd.create_char(3, [0, 14, 21, 23, 17, 14, 0, 0])
+#lcd.create_char(4, [31, 17, 10, 4, 10, 17, 31, 0])
+#lcd.create_char(5, [8, 12, 10, 9, 10, 12, 8, 0])
+#lcd.create_char(6, [2, 6, 10, 18, 10, 6, 2, 0])
+#lcd.create_char(7, [31, 17, 21, 21, 21, 21, 17, 31])
 
 # Show some basic colors.
-lcd.set_color(1.0, 0.0, 0.0)
-lcd.clear()
-lcd.message('RED \x01')
-time.sleep(0.1)
+#lcd.set_color(1.0, 0.0, 0.0)
+#lcd.clear()
+#lcd.message('RED \x01')
+#time.sleep(0.1)
 
-lcd.set_color(0.0, 1.0, 0.0)
-lcd.clear()
-lcd.message('GREEN \x02')
-time.sleep(0.1)
+#lcd.set_color(0.0, 1.0, 0.0)
+#lcd.clear()
+#lcd.message('GREEN \x02')
+#time.sleep(0.1)
 
-lcd.set_color(0.0, 0.0, 1.0)
-lcd.clear()
-lcd.message('BLUE \x03')
-time.sleep(0.1)
+#lcd.set_color(0.0, 0.0, 1.0)
+#lcd.clear()
+#lcd.message('BLUE \x03')
+#time.sleep(0.1)
 
-lcd.set_color(1.0, 1.0, 0.0)
-lcd.clear()
-lcd.message('YELLOW \x04')
-time.sleep(0.1)
+#lcd.set_color(1.0, 1.0, 0.0)
+#lcd.clear()
+#lcd.message('YELLOW \x04')
+#time.sleep(0.1)
 
-lcd.set_color(0.0, 1.0, 1.0)
-lcd.clear()
-lcd.message('CYAN \x05')
-time.sleep(0.1)
+#lcd.set_color(0.0, 1.0, 1.0)
+#lcd.clear()
+#lcd.message('CYAN \x05')
+#time.sleep(0.1)
 
-lcd.set_color(1.0, 0.0, 1.0)
-lcd.clear()
-lcd.message('MAGENTA \x06')
-time.sleep(0.1)
+#lcd.set_color(1.0, 0.0, 1.0)
+#lcd.clear()
+#lcd.message('MAGENTA \x06')
+#time.sleep(0.1)
 
-lcd.set_color(1.0, 1.0, 1.0)
-lcd.clear()
-lcd.message('WHITE \x07')
-time.sleep(0.1)
+#lcd.set_color(1.0, 1.0, 1.0)
+#lcd.clear()
+#lcd.message('WHITE \x07')
+#time.sleep(0.1)
 
 # Define main button handler (interrupt callback function)
 def my_callback(channel):
@@ -72,11 +72,11 @@ lcd.clear()
 lcd.message('Hallo Wouter\n(druk op knop)')
 
 # Make list of button value, text, and backlight color.
-buttons = ((LCD.SELECT, 'Select', (1, 1, 1)),
-           (LCD.LEFT, 'Left', (1, 0, 0)),
-           (LCD.UP, 'Up', (0, 0, 1)),
-           (LCD.DOWN, 'Down', (0, 1, 0)),
-           (LCD.RIGHT, 'Right', (1, 0, 1)))
+#buttons = ((LCD.SELECT, 'Select', (1, 1, 1)),
+#           (LCD.LEFT, 'Left', (1, 0, 0)),
+#           (LCD.UP, 'Up', (0, 0, 1)),
+#           (LCD.DOWN, 'Down', (0, 1, 0)),
+#           (LCD.RIGHT, 'Right', (1, 0, 1)))
 
 print('Press Ctrl-C to quit.')
 
@@ -84,13 +84,13 @@ print('Press Ctrl-C to quit.')
 # Loop through each button and check if it is pressed (for use in specific code later)
 try:
     while(True):
-        for button in buttons:
-            if lcd.is_pressed(button[0]):
+#        for button in buttons:
+#            if lcd.is_pressed(button[0]):
             # Button is pressed, change the message and backlight.
-                lcd.clear()
-                lcd.message(button[1])
-                lcd.set_color(button[2][0], button[2][1], button[2][2])
-
+#                lcd.clear()
+#                lcd.message(button[1])
+#               lcd.set_color(button[2][0], button[2][1], button[2][2])
+        print("in loop")
 
 
 # This open() may hang indefinitely
